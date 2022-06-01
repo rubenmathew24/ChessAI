b7ab1a9601f0e65965579ea12ce8fd0fd4d93f7a
Ananya <ananya.wjhs@gmail.com>
2022-05-31 21:35:04 -0700

    Update Knight.java

--- GUI/Knight.java
+++ GUI/Knight.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 import java.util.ArrayList;
 class Knight extends GamePiece{
 	public Knight(int pos_, boolean hasMoved_, boolean pieceColor_){
-			super(pos_, hasMoved_, pieceColor_);
+		super(pos_, hasMoved_, pieceColor_);
 	}
 	
 	public int[] possibleMoves(HashMap<Integer,GamePiece> board){
